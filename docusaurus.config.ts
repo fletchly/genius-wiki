474586e098d81689ed00641b6630505b96a74ab2
fletchly <owenfletchfletchly@gmail.com>
2025-12-11 14:20:43 -0700

    Update project URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://fletchly.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/genius-wiki/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
