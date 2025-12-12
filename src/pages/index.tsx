a30dc0c4f01973db8686de72dc8190c31b47a501
fletchly <owenfletchfletchly@gmail.com>
2025-12-12 11:58:16 -0700

    Add demo video to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,54 @@ function HomepageHeader() {
   );
 }
 
+function DemoVideo() {
+  return (
+    <section className="padding-vert--xl">
+      <div className="container">
+        {/* Demo heading */}
+        <div className="text--center margin-bottom--lg">
+          <Heading as="h2">Demo</Heading>
+        </div>
+
+        {/* Perfectly centered + nicely sized video */}
+        <div className="row">
+          <div className="col col--8 col--offset-2">
+            {/* Change col--8 → col--10 for a bit wider, col--6 for narrower */}
+            <div
+              style={{
+                position: "relative",
+                paddingBottom: "56.25%", // 16:9
+                height: 0,
+                borderRadius: "12px",
+                overflow: "hidden",
+                boxShadow: "0 10px 30px rgba(0,0,0,0.15)",
+              }}
+            >
+              <iframe
+                src="https://www.youtube.com/embed/xHKZ-8uYC8w"
+                title="Genius Demo"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  border: 0,
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <p className="text--center margin-top--md text--italic">
+          See Genius in action
+        </p>
+      </div>
+    </section>
+  );
+}
+
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -39,6 +87,7 @@ export default function Home(): ReactNode {
     >
       <HomepageHeader />
       <main>
+        <DemoVideo />
         <HomepageFeatures />
       </main>
     </Layout>
